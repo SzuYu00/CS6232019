a21b70632cfe4014473457082c07a591d0a1f608
SzuYu00 <52687922+SzuYu00@users.noreply.github.com>
2019-07-12 10:01:17 -0400

    Update Group3AcidDemo2.java

--- Group3AcidDemo2.java
+++ Group3AcidDemo2.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
+// for MySQL
 public class Group3AcidDemo2 {
 
 	public static void main(String args[]) throws SQLException, IOException, ClassNotFoundException {
